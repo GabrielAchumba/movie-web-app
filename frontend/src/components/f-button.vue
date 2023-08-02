0f025bb7abdfb2d85a21e396948895a9219f3365
GabrielAchumba <achumba.gabriel@yahoo.com>
2023-08-02 14:13:51 +0100

    lesson-03

---
+++ frontend/src/components/f-button.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <q-btn label="Incerement" class="bg-yellow"></q-btn>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,23 +1,42 @@
 <template>
-  <q-page  :class="comClassess">
+  <q-page :class="comClassess">
     <img
+      ref="logo1"
+      v-show="x < 4"
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
-    <p>{{ showLetter("Gabriel Achumba") }}</p>
+
+    <img v-show="x >= 4"
+      alt="Quasar logo"
+      src="~assets/newwaysg.png"
+      style="width: 200px; height: 200px"
+    >
+
+    <p ref="paragraph1" class="parag">{{ showLetter("Gabriel Achumba") }}</p>
+    <p class="parag">I am am am</p>
+    <!-- 
     <input type="text"  v-model="userName" @change="onUserNameChanged"/>
     <q-btn label="Click Me" @click.once="clickMe"
     @mouseover="onMouseHover"
     @mouseout="onMouseOut"
     :style="buttonStyle"/>
-    <q-btn @click="incrementNumber" label="Incerement" class="bg-yellow"></q-btn>
+     -->
+     <q-btn @click="incrementNumber" label="Incerement" class="bg-yellow"></q-btn>
+     <MyButton class="q-pa-sm q-ma-md"/>
+     <ul>
+      <li v-for="(alphabet, index) in alphabets"><p>{{ index }}  {{ alphabet }}</p></li>
+     </ul>
   </q-page>
 </template>
 
 <script>
 //class="flex flex-center"
+import MyButton from "components/f-button.vue"
+//import MyButton from "../components/f-button.vue"
 export default {
+  name: 'PageIndex',
   computed:{
     comClassess(){
       var context = this;
@@ -32,15 +51,9 @@ export default {
       }
     }
   },
-  /* computed:{
-    comClassess(){
-      var context = this;
-      return {
-        [`text-red`]: context.text_color
-      }
-    }
-  }, */
-  name: 'PageIndex',
+  components:{
+    MyButton,
+  },
   data(){
     return {
       imagePath: "~assets/quasar-logo-vertical.svg",
@@ -49,13 +62,17 @@ export default {
       buttonStyle: "background:black; color:red;",
       text_color: true,
       x: 1,
+      alphabets: ["A", "B", "C", "D"]
     }
   },
   methods:{
     incrementNumber(){
       var context = this;
       context.x++
-      console.log(context.x)
+      var refs = this.$refs
+      var logo1 = this.$refs.logo1
+      logo1.style= "width: 400px; height: 400px;"
+      console.log(logo1)
     },
     showLetter(name){
       return "My name is " + name;
@@ -76,3 +93,10 @@ export default {
   }
 }
 </script>
+
+<style>
+
+#parag {
+  font-size:smaller;
+}
+</style>
