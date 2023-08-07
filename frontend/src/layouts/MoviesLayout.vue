ffdcdc882238f0f07563b6a416c34b6b1acde13d
GabrielAchumba <achumba.gabriel@yahoo.com>
2023-08-07 12:24:17 +0100

    updates

--- frontend/src/layouts/MoviesLayout.vue
+++ frontend/src/layouts/MoviesLayout.vue
@@ -76,20 +76,7 @@ export default {
             }
         },
     },
-
-/* directives: {
-  selectAll: {
-    inserted(el) {
-      let input = el.querySelector(".q-field__native");
-      input.addEventListener("focus", () => {
-        if (input.value.length) {
-          input.select();
-        }
-      });
-    },
-  },
-}, */
-created() {
+    created() {
       window.addEventListener("resize", this.onResize);
   },
   destroyed() {
